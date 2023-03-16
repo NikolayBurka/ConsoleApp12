792b260c0b4ac44776fa0c22ba701a01b6cb4e1c
NikolayBurka <125860622+NikolayBurka@users.noreply.github.com>
2023-03-16 12:29:24 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,30 @@
 ﻿//Найти максимальный элемент одномерного массива
+//int[] array = { 8, -1, 4, 16, -5, 3 };
+//int max = array[0];int indexmax = 0;
+//for(int i=0;i<array.Length; i++)
+//{
+//    if (array[i] > max)
+//    {
+//        max = array[i]; indexmax = i;
+//    }
+//}
+//Console.WriteLine($"max={max} indexmax={indexmax}");
+
+// 3 zadanie
+
+int[] array = { 8, -1, 4, 16, -5, 3 };
+int min = array[0]; int indexmin = 0;
+int max = array[0]; int indexmax = 0;
+for (int i = 0; i < array.Length; i++)
+{  
+    if (array[i] < min)
+    {
+        min = array[i]; indexmin = i;
+    }
+    if (array[i] > max)
+    {
+        max = array[i]; indexmax = i;
+    }
+}
+Console.WriteLine($"min={min} indexmin={indexmin}");
+Console.WriteLine($"max={max} indexmax={indexmax}");
